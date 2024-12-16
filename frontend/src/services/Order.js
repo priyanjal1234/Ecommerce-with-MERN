84888f7d7e28d2d02bc2d79e1107a57fddde7c2b
priyanjal1234 <priyanjal362@gmail.com>
2024-12-16 13:18:20 +0530

    Update Order.js

--- frontend/src/services/Order.js
+++ frontend/src/services/Order.js
@@ -3,7 +3,7 @@ import api from "./api";
 class OrderService {
   constructor() {
     this.api = api;
-    this.baseUrl = "http://localhost:3000/api";
+    this.baseUrl = "https://ecommerce-with-mern-backend.onrender.com/api";
   }
 
   async createOrder({
