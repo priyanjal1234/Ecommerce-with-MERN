df7fe61434c10569eb553676c1648e402eacf5e5
priyanjal1234 <priyanjal362@gmail.com>
2024-12-16 13:18:07 +0530

    Update Payment.js

--- frontend/src/services/Payment.js
+++ frontend/src/services/Payment.js
@@ -3,7 +3,7 @@ import api from "./api";
 class PaymentService {
   constructor() {
     this.api = api;
-    this.baseUrl = "http://localhost:3000/api";
+    this.baseUrl = "https://ecommerce-with-mern-backend.onrender.com/api";
   }
 
   async createPaymentIntent(amount) {
