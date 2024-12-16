b2be9bf73f60931607c36390aa03feb04c3fb789
priyanjal1234 <priyanjal362@gmail.com>
2024-12-16 14:17:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://ecommerce-with-mern-frontend.onrender.com',
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 
